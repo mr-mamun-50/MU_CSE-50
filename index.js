293119b0809bdf1932f2377590d78f2cd9b6c160
Mamunur Rashid Mamun <mrmamun20162017@gmail.com>
2021-07-25 19:34:39 +0600

    worked on footer, bus schedule, faculty info, routine, homepage, course section

---
+++ index.js
@@ -0,0 +1,167 @@
+var now = new Date();
+var day = now.getDay();
+
+var todaysCR = document.getElementById("tRoutineHeading").innerHTML;
+
+if (day == 0) {
+    document.getElementById("tRoutineHeading").innerHTML = todaysCR + "(Sunday)";
+
+    document.getElementById("BsecMCF").innerHTML = "10:30 AM";
+    document.getElementById("BsecDM").innerHTML = "2:00 PM";
+
+    document.getElementById("CsecDM").innerHTML = "2:00 PM";
+
+    document.getElementById("DsecDM").innerHTML = "12:30 PM";
+} else if (day == 1) {
+    document.getElementById("tRoutineHeading").innerHTML = todaysCR + "(Monday)";
+
+    document.getElementById("AsecBEE").innerHTML = "10:00 AM";
+
+    document.getElementById("BsecBEE").innerHTML = "10:00 AM";
+
+    document.getElementById("CsecBEE").innerHTML = "2:00 PM";
+
+    document.getElementById("DsecBEE").innerHTML = "2:00 PM";
+} else if (day == 2) {
+    document.getElementById("tRoutineHeading").innerHTML = todaysCR + "(Tuesday)";
+
+    document.getElementById("AsecMCF").innerHTML = "2:00 PM";
+
+    document.getElementById("BsecMCF").innerHTML = "10:30 AM";
+
+    document.getElementById("CsecMCF").innerHTML = "10:30 AM";
+
+    document.getElementById("DsecMCF").innerHTML = "10:30 AM";
+} else if (day == 3) {
+    document.getElementById("tRoutineHeading").innerHTML = todaysCR + "(Wednesday)";
+
+    document.getElementById("AsecBEE").innerHTML = "10:30 AM";
+    document.getElementById("AsecDM").innerHTML = "6:30 PM";
+
+    document.getElementById("BsecBEE").innerHTML = "10:30 AM";
+
+    document.getElementById("CsecBEE").innerHTML = "2:00 PM";
+
+    document.getElementById("DsecBEE").innerHTML = "2:00 PM";
+    document.getElementById("DsecDM").innerHTML = "10:30 AM";
+
+} else if (day == 4) {
+    document.getElementById("tRoutineHeading").innerHTML = todaysCR + "(Thursday)";
+
+    document.getElementById("AsecMCF").innerHTML = "2:00 PM";
+
+    document.getElementById("BsecDM").innerHTML = "2:00 PM";
+
+    document.getElementById("CsecMCF").innerHTML = "10:30 AM";
+    document.getElementById("CsecDM").innerHTML = "2:00 PM";
+
+    document.getElementById("DsecMCF").innerHTML = "10:30 AM";
+
+} else if (day == 5) {
+    document.getElementById("tRoutineHeading").innerHTML = todaysCR + "(Friday)";
+} else if (day == 6) {
+    document.getElementById("tRoutineHeading").innerHTML = todaysCR + "(Saturday)";
+
+    document.getElementById("AsecP200").innerHTML = "3:30 PM";
+    document.getElementById("AsecDM").innerHTML = "6:30 PM";
+
+    document.getElementById("BsecP200").innerHTML = "3:30 PM";
+
+    document.getElementById("CsecP200").innerHTML = "6:30 PM";
+
+    document.getElementById("DsecP200").innerHTML = "6:30 PM";
+}
+
+
+
+function formLoad() {
+
+    var myForm = document.getElementById("myform");
+    var msgSccs = document.getElementById("subMsg");
+    var msgErr = document.getElementById("errMsg");
+
+    if (submitted == true) {
+        myForm.reset();
+        msgSccs.innerHTML = "🗸 Thanks For Your Feedback";
+        msgSccs.classList.add("msgSccs");
+    } else {
+        myForm.reset();
+        msgErr.innerHTML = "✖ Error! Please Try Again";
+        msgErr.classList.add("msgErr");
+    }
+
+    setTimeout(function() {
+
+        document.getElementById("subMsg", "errMsg").innerHTML = "";
+        msgSccs.classList.remove("msgSccs");
+        msgErr.classList.remove("msgErr");
+    }, 5000);
+}
+
+function contFormLoad() {
+
+    var myForm = document.getElementById("myform");
+    var msgSccs = document.getElementById("subMsg");
+    var msgErr = document.getElementById("errMsg");
+
+    if (submitted == true) {
+        myForm.reset();
+        msgSccs.innerHTML = "🗸 Your message has been sent successfully";
+        msgSccs.classList.add("msgSccs");
+    } else {
+        myForm.reset();
+        msgErr.innerHTML = "✖ Error! Please Try Again";
+        msgErr.classList.add("msgErr");
+    }
+
+    setTimeout(function() {
+
+        document.getElementById("subMsg", "errMsg").innerHTML = "";
+        msgSccs.classList.remove("msgSccs");
+        msgErr.classList.remove("msgErr");
+    }, 5000);
+}
+
+
+var h1 = [
+    "Materials of 50 batch is now together",
+    "Best learning place for juniors",
+    "Best tutorial collection on various topics",
+    "Welcome to <br>MU CSE 50"
+];
+var cnt = 0;
+var headerH1 = document.getElementById("headerH1");
+
+setInterval(function() {
+    headerH1.innerHTML = h1[cnt];
+    cnt++;
+
+    if (cnt >= h1.length) {
+        cnt = 0;
+    }
+}, 4000);
+
+
+
+
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Skils', 'Percentage of total students'],
+        ['Competitive Programmer', 25],
+        ['Web Developer', 35],
+        ['Android Developer', 10],
+        ['Graphic Designer', 25],
+        ['Others', 5]
+    ]);
+
+    var options = {
+        title: 'Statistics at a glance',
+        is3D: true,
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+    chart.draw(data, options);
+}
