d59a66606510ac6ee15f9db036f23d29f813ff63
Mamunur Rashid Mamun <mrmamun20162017@gmail.com>
2021-07-29 19:40:01 +0600

    1-Stop form working correctly

--- index.js
+++ index.js
@@ -100,23 +100,47 @@ function formLoad() {
 
 function contFormLoad() {
 
-    var myForm = document.getElementById("myform");
-    var msgSccs = document.getElementById("subMsg");
-    var msgErr = document.getElementById("errMsg");
+    var myContForm = document.getElementById("contform");
+    var msgSccs = document.getElementById("contSubMsg");
+    var msgErr = document.getElementById("contErrMsg");
 
     if (submitted == true) {
-        myForm.reset();
+        myContForm.reset();
         msgSccs.innerHTML = "🗸 Your message has been sent successfully";
         msgSccs.classList.add("msgSccs");
     } else {
-        myForm.reset();
+        myContForm.reset();
         msgErr.innerHTML = "✖ Error! Please Try Again";
         msgErr.classList.add("msgErr");
     }
 
     setTimeout(function() {
 
-        document.getElementById("subMsg", "errMsg").innerHTML = "";
+        document.getElementById("contSubMsg", "contErrMsg").innerHTML = "";
+        msgSccs.classList.remove("msgSccs");
+        msgErr.classList.remove("msgErr");
+    }, 5000);
+}
+
+function oneStopFormLoad() {
+
+    var myOneForm = document.getElementById("myOneForm");
+    var msgSccs = document.getElementById("oneSubMsg");
+    var msgErr = document.getElementById("oneErrMsg");
+
+    if (submitted == true) {
+        myOneForm.reset();
+        msgSccs.innerHTML = "🗸 Your mail has been successfully sent to 1-Stop service";
+        msgSccs.classList.add("msgSccs");
+    } else {
+        myOneForm.reset();
+        msgErr.innerHTML = "✖ Error! Please Try Again";
+        msgErr.classList.add("msgErr");
+    }
+
+    setTimeout(function() {
+
+        document.getElementById("oneSubMsg", "oneErrMsg").innerHTML = "";
         msgSccs.classList.remove("msgSccs");
         msgErr.classList.remove("msgErr");
     }, 5000);
